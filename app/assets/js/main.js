e7525a73e2f57b98c33fe96d31dbb05581af54c2
EricDupertuis <dupertuis.eric@gmail.com>
2015-03-10 23:49:34 +0100

    More work and font change

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,11 +1,3 @@
-$(window).scroll(function() {
-    if ($(this).scrollTop() > $("#about").offsetTop) {
-        $( "#nav" ).fadeIn();
-    } else {
-        $( "#nav" ).fadeOut();
-    }
-});
-
 $('a[href*=#]').on('click', function(event){
     event.preventDefault();
     $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
