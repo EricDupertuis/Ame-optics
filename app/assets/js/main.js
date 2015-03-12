85d4bfa0fae14c0eb974e633e840308d6dc66130
EricDupertuis <dupertuis.eric@gmail.com>
2015-03-12 22:40:09 +0100

    responsive navigation

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,4 +1,19 @@
-$('a[href*=#]').on('click', function(event){
+$( document ).ready(function(){
+    $('.navList a').click(function(){
+        $('.navList').slideToggle();    
+    });
+
+    if ($(window).width() < 960) {
+        $('.navList').slideUp();
+    }
+});
+
+$('.navList a[href*=#]').on('click', function(event){
     event.preventDefault();
     $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+});
+
+$('.navToogle').click(function(e){
+    e.preventDefault();
+    $('.navList').slideToggle();
 });
