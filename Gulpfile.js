228a5d4b03c515cc40a4f71904f846457a5f8ce2
EricDupertuis <dupertuis.eric@gmail.com>
2015-03-17 23:32:24 +0100

    base gulp plugins list

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,4 +1,13 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var imagemin = require('gulp-imagemin');
+var prefix = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var plumber = require('gulp-plumber');
+var uglify = require('gulp-uglify');
+var minify = require('gulp-minify-css');
+
 
 gulp.task('default', function() {
     // place code for your default task here
