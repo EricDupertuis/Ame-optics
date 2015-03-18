693c6f2d0c939ad29cf8ab2b8650aa331739aa8c
EricDupertuis <dupertuis.eric@gmail.com>
2015-03-18 10:58:36 +0100

    ignore node_modules

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var concat = require('gulp-concat');
 var imagemin = require('gulp-imagemin');
 var prefix = require('gulp-autoprefixer');
@@ -7,8 +8,17 @@ var watch = require('gulp-watch');
 var plumber = require('gulp-plumber');
 var uglify = require('gulp-uglify');
 var minify = require('gulp-minify-css');
+var filesize = require('gulp-filesize');
 
 
 gulp.task('default', function() {
     // place code for your default task here
+});
+
+gulp.task('js', function() {
+    return gulp.src('app/assets/js/*.js')
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/assets/js/main.js'))
+        .on('error', gutil.log)
 });
